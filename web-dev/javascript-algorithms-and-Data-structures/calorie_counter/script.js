d39cd3d570775fd01c3a8cc551e070304fff547f
Hexbyte <islamamara062@gmail.com>
2025-09-01 07:19:41 +0000

    getCaloriesFromInputs completed

--- web-dev/javascript-algorithms-and-Data-structures/calorie_counter/script.js
+++ web-dev/javascript-algorithms-and-Data-structures/calorie_counter/script.js
@@ -36,3 +36,25 @@ function addEntry() {
 }
 
 addEntryButton.addEventListener("click", addEntry)
+
+
+
+
+function getCaloriesFromInputs(list) {
+  let calories = 0;
+
+  for (const item of list) {
+    const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch}`);
+      isError = true;
+      return null;
+      
+    }
+
+    calories += Number(currVal);
+    
+  }
+  return calories
+}
