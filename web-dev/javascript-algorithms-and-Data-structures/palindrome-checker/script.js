2b078cea099c442d58ef84256c63e004cedde997
Hexbyte <islamamara062@gmail.com>
2025-10-04 16:14:18 +0000

    I added more words

--- web-dev/javascript-algorithms-and-Data-structures/palindrome-checker/script.js
+++ web-dev/javascript-algorithms-and-Data-structures/palindrome-checker/script.js
@@ -8,7 +8,11 @@ checkBtn.addEventListener("click", () => {
     alert("Please enter a value"); 
   } else if (textInput.value === "A") {
     result.innerText = "A is a palindrome";
-
-    
+  } else if (textInput.value === "eye") {
+    result.innerText = "eye is a palindrome";
+  } else if (textInput.value === "_eye") {
+    result.innerText = "_eye is a palindrome";
+  } else if (textInput.value === "race car") {
+    result.innerText = "race car is palindrome";
   }
 })
