07329f60396e08853d9fdd480822c27d3f8d38ba
Hexbyte <islamamara062@gmail.com>
2025-10-07 22:12:02 +0000

    i added a better eventlistener (palindrome checker)

--- web-dev/javascript-algorithms-and-Data-structures/palindrome-checker/script.js
+++ web-dev/javascript-algorithms-and-Data-structures/palindrome-checker/script.js
@@ -2,4 +2,21 @@ const checkBtn = document.getElementById("check-btn");
 const textInput = document.getElementById("text-input");
 const result = document.getElementById("result");
 
+checkBtn.addEventListener("click", () => {
+  const text = textInput.value.trim();
+  if (text === "") {
+    alert("Please Enter a value");  
+    return;
+  }
 
+  const cleanText = text.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  const reversed = cleanText.split("").reverse().join();
+
+
+   
+  
+  ;
+  
+
+
+});
