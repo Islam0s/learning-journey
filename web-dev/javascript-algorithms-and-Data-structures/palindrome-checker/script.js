73a6ff628095da7486a209c2ac5feab5e2c10ab8
Hexbyte <islamamara062@gmail.com>
2025-10-07 21:42:44 +0000

    Started over in JS file (palindrome checker)

--- web-dev/javascript-algorithms-and-Data-structures/palindrome-checker/script.js
+++ web-dev/javascript-algorithms-and-Data-structures/palindrome-checker/script.js
@@ -3,16 +3,3 @@ const textInput = document.getElementById("text-input");
 const result = document.getElementById("result");
 
 
-checkBtn.addEventListener("click", () => {
-  if (textInput.value === "") {
-    alert("Please enter a value"); 
-  } else if (textInput.value === "A") {
-    result.innerText = "A is a palindrome";
-  } else if (textInput.value === "eye") {
-    result.innerText = "eye is a palindrome";
-  } else if (textInput.value === "_eye") {
-    result.innerText = "_eye is a palindrome";
-  } else if (textInput.value === "race car") {
-    result.innerText = "race car is palindrome";
-  }
-})
